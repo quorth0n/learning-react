2889cc1b032a91015469c2f3dff2d51d4692c8ec
lain <lpoflynn@protonmail.ch>
2018-06-20 20:36:17 -0700

    Completed chapter 2

---
+++ Ex_Files_React_EssT/Ch02/02_06/start/src/lib.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import text from './titles.json'
+
+export const hello = (
+    <h1 id='title'
+        className='style'>
+        {text.hello}
+    </h1>
+)
+
+export const goodbye = (
+    <h1 id='title'
+        className='goodbye'>
+        {text.goodbye}
+    </h1>
+)
